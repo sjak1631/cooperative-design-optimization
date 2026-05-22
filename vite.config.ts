93fe83b08bdfd46e6f719ca83496958db38aafbd
sjak1631 <akira.shoji.2004@gmail.com>
2026-05-22 11:27:02 +0900

    chore: update Vite config for production base and server settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/ex3-season2-slide-app/' : '/',
   plugins: [react()],
+  server: {
+    port: 5174,
+    host: '0.0.0.0',
+  },
 })
